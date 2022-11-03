07c8f4223e3bdf7b735c17019dd16410cd1926fd
VeneeVincents <93650183+VeneeVincents@users.noreply.github.com>
2022-11-03 09:53:45 +0000

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,7 +18,7 @@ const Footer = () => {
     <List items={footerList1} mt={false} />
     <List items={footerList2} mt />
     <List items={footerList3} mt />
-    <p className='text-gray-400 text-sm mt-5'>2022 Video TikTik</p>
+    <p className='text-gray-400 text-sm mt-5'>2022 Trending Videos</p>
     </div>
   )
 }
